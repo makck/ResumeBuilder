06125ca7b4746dd3945f6a45ab7721df06142991
makck <joel.mak32@gmail.com>
2022-05-13 15:16:11 +0800

    Create user tables migrations

---
+++ migrations/20220513065245-create-users-table.js
@@ -0,0 +1,45 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      first_name: {
+        type: Sequelize.STRING,
+      },
+      last_name: {
+        type: Sequelize.STRING,
+      },
+      email: {
+        type: Sequelize.STRING,
+      },
+      github_account: {
+        type: Sequelize.STRING,
+      },
+      linkedin: {
+        type: Sequelize.STRING,
+      },
+      mobile_number: {
+        type: Sequelize.INTEGER,
+      },
+      profile_image: {
+        type: Sequelize.STRING,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('users');
+  },
+};
