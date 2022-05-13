d46f1aac14a4ba46c1b9305f64dbd21aa98dc3cb
makck <joel.mak32@gmail.com>
2022-05-13 14:03:11 +0800

    Update db config

--- config/config.js
+++ config/config.js
@@ -1,8 +1,8 @@
 module.exports = {
   development: {
-    username: '<YOUR_USER_NAME>',
+    username: 'Chan Keet',
     password: null,
-    database: '<YOUR_APP_NAME>_development',
+    database: 'resume_builder_development',
     host: '127.0.0.1',
     dialect: 'postgres',
   },
