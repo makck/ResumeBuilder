46e518bd036ff5c67f1674af778400332896e816
makck <joel.mak32@gmail.com>
2022-05-13 16:19:14 +0800

    Create user model

---
+++ models/user.mjs
@@ -0,0 +1,45 @@
+export default function initUserModel(sequelize, DataTypes) {
+  return sequelize.define(
+    'user',
+    {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      first_name: {
+        type: DataTypes.STRING,
+      },
+      last_name: {
+        type: DataTypes.STRING,
+      },
+      email: {
+        type: DataTypes.STRING,
+      },
+      github_account: {
+        type: DataTypes.STRING,
+      },
+      linkedin: {
+        type: DataTypes.STRING,
+      },
+      mobile_number: {
+        type: DataTypes.INTEGER,
+      },
+      profile_image: {
+        type: DataTypes.STRING,
+      },
+      created_at: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+    },
+    {
+      underscored: true,
+    },
+  );
+}
