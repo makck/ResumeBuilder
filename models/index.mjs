acd297be4e2b3150235fee0d9e12f4d9b33795ca
makck <joel.mak32@gmail.com>
2022-05-13 16:35:36 +0800

    Update with new model relations

--- models/index.mjs
+++ models/index.mjs
@@ -2,6 +2,10 @@ import { Sequelize } from 'sequelize';
 import url from 'url';
 import allConfig from '../config/config.js';
 
+import initUserModel from './user.mjs';
+import initResumeModel from './resume.mjs';
+import initIndustryModel from './industry.mjs';
+
 const env = process.env.NODE_ENV || 'development';
 
 const config = allConfig[env];
@@ -29,6 +33,16 @@ if (env === 'production') {
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
+
+db.User = initUserModel(sequelize, Sequelize.DataTypes);
+db.Resume = initResumeModel(sequelize, Sequelize.DataTypes);
+db.Industry = initIndustryModel(sequelize, Sequelize.DataTypes);
+
+db.User.belongsTo(db.Resume);
+db.Resume.hasMany(db.User);
+
+db.Resume.belongsToMany(db.Industry, { through: 'resumes_industries' });
+db.Industry.belongsToMany(db.Resume, { through: 'resumes_industries' });
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
