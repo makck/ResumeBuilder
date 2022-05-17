12b2baf197cd9d33872cfb3b3724d31d4b10007c
makck <joel.mak32@gmail.com>
2022-05-17 20:51:22 +0800

    Refactor to utilise FormCareerSummary component

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import NavBar from './components/NavBar.jsx';
 // User form components
 import FormUserProfile from './components/FormUserProfile.jsx';
 import FormEducation from './components/FormEducation.jsx';
+import FormCareerSummary from './components/FormCareerSummary.jsx';
 
 // Reusme rendering components
 import ResumeSideBar from './components/ResumeSideBar.jsx';
 
 export default function App() {
+  // User Profile
   const [userProfile, setUserProfile] = useState({
     first_name: 'test',
     last_name: 'test2',
@@ -18,12 +20,6 @@ export default function App() {
     github_account: '',
     mobile_number: 0,
   });
-
-  const [educationHistory, setEducationHistory] = useState([{
-    qualification: '',
-    institution: '',
-    year_duration: '',
-  }]);
 
   const userProfileSubmit = (event) => {
     event.preventDefault();
@@ -38,6 +34,14 @@ export default function App() {
       mobile_number: event.target.mobile_number.value,
     }));
   };
+
+  // Education History
+
+  const [educationHistory, setEducationHistory] = useState([{
+    qualification: '',
+    institution: '',
+    year_duration: '',
+  }]);
 
   const handleEducationFormAdd = () => {
     setEducationHistory([...educationHistory, {
@@ -59,17 +63,43 @@ export default function App() {
     setEducationHistory([...educationHistory]);
   };
 
+  // Career Summary
+  const [careerSummary, setCareerSummary] = useState('');
+
+  const handleSummaryFormChange = (event, index) => {
+    const summaryValue = event.target.value;
+    let tempString = careerSummary;
+    tempString = summaryValue;
+    setCareerSummary(tempString);
+  };
+
   return (
     <div>
       <NavBar />
       <div className="container">
         <FormUserProfile userProfileSubmit={userProfileSubmit} />
         <FormEducation educationHistorySubmit={educationHistorySubmit} educationHistory={educationHistory} handleEducationFormChange={handleEducationFormChange} handleEducationFormAdd={handleEducationFormAdd} />
+        <FormCareerSummary handleSummaryFormChange={handleSummaryFormChange} />
 
         <div className="row mt-4">
           <h1>Resume Preview</h1>
           <div className="wrapper mt-lg-5">
             <ResumeSideBar userProfile={userProfile} educationHistory={educationHistory} />
+
+            <div className="main-wrapper">
+
+              <section className="section summary-section">
+                <h2 className="section-title">
+                  <span className="icon-holder"><i className="fas fa-user" /></span>
+                  Career Profile
+                </h2>
+                <div className="summary">
+                  <p>
+                    {careerSummary}
+                  </p>
+                </div>
+              </section>
+            </div>
           </div>
         </div>
       </div>
