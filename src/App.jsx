a7e037c1e848b388e2f3313047fd948dfe36df4d
makck <joel.mak32@gmail.com>
2022-05-17 19:59:18 +0800

    Refactor to use FormEducation component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import NavBar from './components/NavBar.jsx';
 // User form components
 import FormUserProfile from './components/FormUserProfile.jsx';
+import FormEducation from './components/FormEducation.jsx';
 
 // Reusme rendering components
 import ResumeSideBar from './components/ResumeSideBar.jsx';
@@ -63,38 +64,7 @@ export default function App() {
       <NavBar />
       <div className="container">
         <FormUserProfile userProfileSubmit={userProfileSubmit} />
-
-        <div className="row">
-          <h1 className="page-title">Education</h1>
-
-          <div>
-            <form className="form-inline" onSubmit={educationHistorySubmit}>
-
-              {educationHistory.map((entry, index) => (
-                <div key={`education-${index}`} className="row mb-3">
-                  <div className="col">
-                    <input className="form-control" name="qualification" type="text" placeholder="Qualification" onChange={(event) => handleEducationFormChange(event, index)} />
-                  </div>
-                  <div className="col">
-                    <input className="form-control" name="institution" type="text" placeholder="Institution" onChange={(event) => handleEducationFormChange(event, index)} />
-                  </div>
-                  <div className="col">
-                    <input className="form-control" name="year_duration" type="text" placeholder="Year Duration" onChange={(event) => handleEducationFormChange(event, index)} />
-                  </div>
-                </div>
-              ))}
-
-              <div className="row">
-                <button className="col-4 btn btn-secondary mb-2" type="button" onClick={handleEducationFormAdd}>Add another education entry</button>
-              </div>
-
-              <div className="row">
-                <button className="col-4 btn btn-secondary mb-2" type="submit">Add Education</button>
-              </div>
-            </form>
-          </div>
-
-        </div>
+        <FormEducation educationHistorySubmit={educationHistorySubmit} educationHistory={educationHistory} handleEducationFormChange={handleEducationFormChange} handleEducationFormAdd={handleEducationFormAdd} />
 
         <div className="row">
           <h1>Resume Preview</h1>
