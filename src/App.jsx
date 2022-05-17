c6c21f17e80212be06133d5b4dad550e19ce6293
makck <joel.mak32@gmail.com>
2022-05-17 14:45:46 +0800

    Refactor Nav Bar to component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 
+import NavBar from './components/NavBar.jsx';
 import ResumeUserProfile from './components/resumeUserProfile.jsx';
 
 export default function App() {
@@ -31,27 +32,7 @@ export default function App() {
 
   return (
     <div>
-
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">Resume Builder</a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon" />
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">Collection</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">Industries</a>
-              </li>
-            </ul>
-
-          </div>
-        </div>
-      </nav>
-
+      <NavBar />
       <div className="container">
         <div className="row">
           <h1 className="page-title">User Profile</h1>
