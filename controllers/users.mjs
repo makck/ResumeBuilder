a7d31c313aa1e8465d847221ebf1a9517ca56946
makck <joel.mak32@gmail.com>
2022-05-31 01:24:33 +0800

    Create users controller

---
+++ controllers/users.mjs
@@ -0,0 +1,22 @@
+export default function initUsersController(db) {
+  const createUser = async (req, res) => {
+    console.log(req.body);
+    const profile_image = 'dummy';
+    const created_at = new Date();
+    const updated_at = new Date();
+    const {
+      first_name, last_name, email, github_account, linkedin, mobile_number,
+    } = req.body;
+    try {
+      const newUser = await db.User.create({
+        first_name, last_name, email, github_account, linkedin, mobile_number, profile_image, created_at, updated_at,
+      });
+
+      res.send({ newUser });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return { createUser };
+}
