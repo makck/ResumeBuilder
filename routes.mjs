4203899a25e493a2a23341b387475c0c597aae95
makck <joel.mak32@gmail.com>
2022-05-31 01:22:33 +0800

    Create create-user route

--- routes.mjs
+++ routes.mjs
@@ -1,6 +1,13 @@
 import { resolve } from 'path';
+import db from './models/index.mjs';
+
+import initUsersController from './controllers/users.mjs';
 
 export default function routes(app) {
+  const UsersController = initUsersController(db);
+
+  app.post('/create-user', UsersController.createUser);
+
   app.get('/', (request, response) => {
     response.sendFile(resolve('dist', 'main.html'));
   });
