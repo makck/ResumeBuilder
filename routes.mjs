f127155ffc2a012c9c3b93a5bbc2d8f2e4676e26
makck <joel.mak32@gmail.com>
2022-05-31 02:25:48 +0800

    Add create-resume route

--- routes.mjs
+++ routes.mjs
@@ -2,11 +2,15 @@ import { resolve } from 'path';
 import db from './models/index.mjs';
 
 import initUsersController from './controllers/users.mjs';
+import initResumesController from './controllers/resumes.mjs';
 
 export default function routes(app) {
   const UsersController = initUsersController(db);
+  const ResumesController = initResumesController(db);
 
   app.post('/create-user', UsersController.createUser);
+
+  app.post('/create-resume', ResumesController.createResume);
 
   app.get('/', (request, response) => {
     response.sendFile(resolve('dist', 'main.html'));
